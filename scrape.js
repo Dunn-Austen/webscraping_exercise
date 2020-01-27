15846b0dc5e8947c3a9a9fc53506720ec20b6a97
Alan Turing <austeyd@gmail.com>
2020-01-26 17:43:26 -0700

    Add scrape code

--- scrape.js
+++ scrape.js
@@ -1,1 +1,20 @@
-https://www.nba.com/scores#/
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: true });
+
+nightmare
+  .goto('https://www.nba.com/scores#/')
+  .evaluate(function() {
+    const teamNameNodes = document.querySelectorAll('.score-tile__team-name');
+    const list = [].slice.call(teamNameNodes);
+    const teamNames = list.map(team => {
+      return team.innerText
+    });
+    return teamNames
+  })
+  .end()
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
